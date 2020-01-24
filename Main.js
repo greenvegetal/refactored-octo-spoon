fabd3bd0b6f0f4a7b1ab5be7c5dc2579b69573f2
greenvegetal <Azhxta@gmail.com>
2020-01-24 19:06:46 +0600

    working state of rps game

--- Main.js
+++ Main.js
@@ -1,45 +1,49 @@
 function computerChoice() {
     const items = ["rock", "paper" , "scissors"];
     return items[Math.floor(Math.random() * items.length)];
-    
 }
-const humanDecision = prompt("Choose one, rock paper or scissors");
+
 function humanChoice () {
-return humanDecision;
+let decision= prompt("Choose one, rock paper or scissors");
+return decision;
 }
 
-;
+const gHumanChoice = humanChoice();
+const gComputerChoice = computerChoice ();
 
-function playRound () 
-{
 
+function playRound () {
 const winDialogue = "You win! 10 points to Gryffindor!";
 const loseDialogue = "You lost to a machine, Quick! Run for your life!";
-const drawDialogue = "Draw! Lets go again.";
-const errorDialogue = "Uh oh, something is wrong";
+const drawDialogue = "Draw! Lets go again!";
+const errorDialogue = "Uh oh, something is wrong!";
 
-if (humanChoice) 
+if (gHumanChoice) 
 { 
-    if (humanChoice == "rock" && computerChoice == "scissors" ||
-        humanChoice == "paper" && computerChoice == "rock" ||  
-        humanChoice == "scissors" && computerChoice == "paper")
+    if (gHumanChoice == "rock" && gComputerChoice == "scissors" ||
+        gHumanChoice == "paper" && gComputerChoice == "rock" ||  
+        gHumanChoice == "scissors" && gComputerChoice == "paper")
         
-        {
+    {
         alert (winDialogue);
-        }
-    else if (humanChoice == "rock" && computerChoice == "paper" || 
-            humanChoice == "paper" && computerChoice == "scissors" ||
-            humanChoice == "scissors" && computerChoice == "rock") 
+    }
+    else if (gHumanChoice == "rock" && gComputerChoice == "paper" || 
+            gHumanChoice == "paper" && gComputerChoice == "scissors" ||
+            gHumanChoice == "scissors" && gComputerChoice == "rock") 
             
-        {
+    {
         alert (loseDialogue);
-        }
-    else if ((humanChoice == "rock" && computerChoice == "rock" ||
-    humanChoice == "paper" && computerChoice == "paper" ||  
-    humanChoice == "scissors" && computerChoice == "scissors")) {
+    }
+    else if ((gHumanChoice == "rock" && gComputerChoice == "rock" ||
+    gHumanChoice == "paper" && gComputerChoice == "paper" ||  
+    gHumanChoice == "scissors" && gComputerChoice == "scissors")) 
+    {
         alert (drawDialogue);
     }
 }
+else {
+    alert (errorDialogue);
+}
 
 }
-playRound();
+playRound(gComputerChoice, gHumanChoice);
